77e57f1918e8bcd8fe117670aae7edfadb4a0a17
Shaikh Aatif <shaikhaatif7557@gmail.com>
2025-08-22 23:57:38 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ const templates = [
         price: 149,
         category: "birthday",
         image: "assets/BD1.jpg",
-        description: "A profesionally Devloped with beutiful theme, fonts, amazing functionality and your fav song in background. Customize and suprise your partner ",
+        description: "A profesionally Devloped with beutiful theme, fonts, amazing functionality and your fav song in background. Customize and suprise your partner.",
         badge: "Popular"
     },
     {
